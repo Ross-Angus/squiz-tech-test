38dda6d2aafaf1f98179aba58003e6144debfe71
Ross Angus <ross.a.angus@gmail.com>
2025-05-20 15:19:06 +0100

    Mobile fix

--- src/components/ClientInfo/ClientInfo.jsx
+++ src/components/ClientInfo/ClientInfo.jsx
@@ -5,15 +5,15 @@ const ClientInfo = ({clientData, colourClass}) => (
   <aside aria-label={clientData.name} className={`card ${colourClass}`}>
     <h2>{clientData.name}</h2>
     <div className={classes.row}>
-      <dl className={classes.stats}>
+      <dl className={classes.clientInfo}>
         <dt>Country of origin</dt>
         <dd className={classes.larger}>{clientData.country}</dd>
       </dl>
-      <dl className={classes.stats}>
+      <dl className={classes.clientInfo}>
         <dt>Industry</dt>
         <dd className={classes.large}>{clientData.industry}</dd>
       </dl>
-      <dl className={classes.stats}>
+      <dl className={classes.clientInfo}>
         <dt>Number of employees</dt>
         <dd className={classes.largest}>{clientData.numberOfEmployees}</dd>
       </dl>
